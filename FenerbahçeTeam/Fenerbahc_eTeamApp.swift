3f18efbaccaf70eae1decc131e385bbb3079a945
ocobanoglu18 <ocobanoglu18@ku.edu.tr>
2022-11-15 12:00:40 +0300

    Initial Commit

---
+++ FenerbahçeTeam/Fenerbahc_eTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fenerbahc_eTeamApp.swift
+//  FenerbahçeTeam
+//
+//  Created by Adnan Cobanoglu on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Fenerbahc_eTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
